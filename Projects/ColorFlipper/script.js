dfc02d51837a13da6a084111c522ab355f68a977
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-11-15 10:55:50 +0530

    Completed color flipper project

---
+++ Projects/ColorFlipper/script.js
@@ -0,0 +1,18 @@
+let main = document.getElementById("main");
+let button = document.querySelector("button");
+let h3 = document.querySelector("h3");
+
+function getRandomColor() {
+  let color = "#";
+  let chars = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    color += chars[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+button.addEventListener("click", () => {
+  let randomColor = getRandomColor();
+  main.style.backgroundColor = randomColor;
+  h3.innerHTML = randomColor;
+});
